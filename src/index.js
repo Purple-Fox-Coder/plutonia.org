2fabfbe322fef2ca359c6b27b932ed6661a51722
Empress Anna <purplefoxcoder@gmail.com>
2025-12-20 21:11:06 +0000

    feat(astro): Move over to using astro
    * feat(zig): Set up Zig/WASM support so I can do more interesting things in the front-end
    
    I do intend to keep this a static website though, given it's cheaper
    
    * feat(astro): Move everything over to astro/tailwind
    
    * fix(typo): Remove additional </strong> end tagthat was not doing anything
    
    * feat(media): Import media and properly apply images
    
    * feat(favicon): Include favicon for site title logo
    
    ---------
    
    Authored-by: Anna Robinson <annatheprogrammer777@protonmail.com>

--- src/index.js
+++
@@ -1,4 +0,0 @@
-var button = document.getElementById('myButton');
-button === null || button === void 0 ? void 0 : button.addEventListener('click', function () {
-    alert('Button was clicked!');
-});
